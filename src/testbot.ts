5712fa2519f1d0dd268cd4f57e0f5012c78c4430
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-03-28 16:07:19 -0400

    Removed empty space

--- src/testbot.ts
+++ src/testbot.ts
@@ -2,8 +2,6 @@ const BotDataManager = require("./DiscordBot/Core/Data/BotDataManager");
 
 const DiscordBot = require("./DiscordBot/Core/DiscordBot");
 
-
-
 const Bot = new DiscordBot.default(BotDataManager.default);
 
 Bot.StartBot();
