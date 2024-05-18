7f44ad9e81f5e34c559b568cc2e45041c73b88f2
MrDNA <Mr.DNAlex.2003@gmail.com>
2024-05-18 00:15:20 -0400

    Commented out code

--- src/testbot.js
+++ src/testbot.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const BotDataManager = require("./DiscordBot/Core/Data/BotDataManager");
-const DiscordBot = require("./DiscordBot/Core/DiscordBot");
-const Bot = new DiscordBot.default(BotDataManager.default);
-Bot.StartBot();
+// const BotDataManager = require("./DiscordBot/Core/Data/BotDataManager");
+// const DiscordBot = require("./DiscordBot/Core/DiscordBot");
+// const Bot = new DiscordBot.default(BotDataManager.default);
+// Bot.StartBot();
